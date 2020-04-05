a9d4ab0c8e629584c0f4ecef99b7a38d27e03b3b
hsong1 <hsong56@wisc.edu>
2020-04-05 15:01:49 -0500

    -add an auc function

---
+++ R/auc.R
@@ -0,0 +1,21 @@
+#'Return an adjusted auc value from a fitted model at py1 ==py1[py1idx]
+#'@param vfit vpudms.fit object
+#'@param py1idx an index of py1 for auc to be returned at
+#'@export
+#'
+auc<-function(vfit,py1idx=NULL){
+  
+  #if py1idx is not null, check py1idx is valid
+  if(!(is.null(py1idx) & py1idx%in%1:length(vfit$py1))){stop("py1idx should be between 1 and length(input$py1)")}
+  #if py1idx is null, py1.opt has to be not a null value
+  if(is.null(py1idx) & is.null(vfit$py1.opt)){stop("py1.opt is NULL")}
+  
+  if(is.null(py1idx)){
+    aucval = with(vfit, roc_curves[[which(py1 == py1.opt)]]$auc)
+  }else{
+    aucval = with(vfit, roc_curves[[py1idx]]$auc)
+  }
+  
+  return(aucval)
+  
+}
