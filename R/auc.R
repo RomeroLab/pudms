024da3408660a7a1bf79bb8a70eb5feed38c4ca1
hsong1 <hsong56@wisc.edu>
2020-04-10 21:50:29 -0500

    - change in auc function

--- R/auc.R
+++ R/auc.R
@@ -1,4 +1,4 @@
-#'Return an adjusted auc value from a fitted model at py1 ==py1[py1idx]
+#'Return an adjusted (average) auc value from a fitted model at py1 ==py1[py1idx]
 #'@param vfit vpudms.fit object
 #'@param py1idx an index of py1 for auc to be returned at
 #'@export
@@ -7,7 +7,7 @@ auc<-function(vfit,py1idx=NULL){
   
   #if py1idx is not null, check py1idx is valid
   if(!is.null(py1idx)){
-    if(py1idx%in%1:length(vfit$py1)) stop("py1idx should be between 1 and length(input$py1)")
+    if(!py1idx%in%1:length(vfit$py1)) stop("py1idx should be between 1 and length(input$py1)")
   }
     
   #if py1idx is null, py1.opt has to be not a null value
